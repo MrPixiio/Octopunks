a0b76e1afb656555cde5b47f98249d3fa742e7c9
MrPixiio <127220611+MrPixiio@users.noreply.github.com>
2024-06-06 14:58:50 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+
+package techtitans;
+import techtitans.view.*;
+
+public class Main{
+    public static void main(String[] args) throws Exception {
+        
+        new Thread(new InterfaceGraphique()).start();
+        
+    }
+}
